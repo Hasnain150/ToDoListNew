c6d79aea2b9838a20aed73833bb8e3b075bbc604
Hasnain150 <90840161+Hasnain150@users.noreply.github.com>
2023-08-31 05:56:42 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+let Addbtn=document.querySelector("#inputBtn");
+let searchBar=document.querySelector("#inputText");
+function AddtoDo(){
+    
+     
+}
+function AddtoDoWithKeyboard(e){
+    if(e.key==="Enter" || e.key===13){
+        if(searchBar.value){
+            let li =document.createElement("li");
+            li.innerHTML=searchBar.value;
+            let div=document.querySelector("#ToDos");
+            div.appendChild(li);
+            let theImage=document.createElement("img");
+            theImage.setAttribute("src","img/cross.png");
+            console.log(theImage)
+            li.appendChild(theImage)
+            searchBar.value="";
+            saveData();
+           
+         }
+         else if(searchBar.value===""){
+            alert("PLEASE ENTER A VALID TO DO");
+         }
+}
+
+    
+}
+Addbtn.addEventListener('click',AddtoDo);
+searchBar.addEventListener('keydown',AddtoDoWithKeyboard);
+const listContainer=document.getElementById("ToDos")
+listContainer.addEventListener("click",function(e)  {
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="IMG"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false)
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+function loadData(){
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+loadData();
